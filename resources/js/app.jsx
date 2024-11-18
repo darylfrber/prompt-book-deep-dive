7fe3fd7eb7996091b3c522445275fbce8ca10608
darylfrber <134004@student.horizoncollege.nl>
2024-11-18 10:52:31 +0100

    Upload of project start

---
+++ resources/js/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+    return <h1>Hello, React in Laravel!</h1>;
+};
+
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);
