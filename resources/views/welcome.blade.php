a46f5d9c502c16347cda8f7510e86c2e45438753
darylfrber <134004@student.horizoncollege.nl>
2024-11-19 15:03:23 +0100

    User profile added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Login</title>
+        <title>Promptbook</title>
+        <script src="https://cdn.tailwindcss.com"></script>
         @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     </head>
     <body>
