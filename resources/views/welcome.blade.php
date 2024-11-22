81ffd2ad7533dda1d5dc4b188ff43b02e82b8dd1
Damian <damianeijlders@gmail.com>
2024-11-22 13:00:04 +0100

    feat: add favicon to the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
         <title>Promptbook</title>
         <script src="https://cdn.tailwindcss.com"></script>
         @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+        <link rel="shortcut icon" href="/images/logo.png" type="image/x-icon">
     </head>
     <body class="bg-gray-50">
         <div id="app"></div>
